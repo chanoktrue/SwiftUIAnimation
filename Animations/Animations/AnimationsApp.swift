bff5ccbdb38e285a7630dd9f8a743238063be2f8
Thongchai Subsaidee <chanoktrue@me.com>
2021-05-24 20:08:39 +0700

    first project

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Thongchai Subsaidee on 24/5/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
